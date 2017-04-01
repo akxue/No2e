e78f0b9f72eafc571c6ab7e8c297815ac101647a
Anthony Xue <anthkx@ucla.edu>
2017-04-01 14:45:02 -0700

    add user+string sending

--- routes.js
+++ routes.js
@@ -7,28 +7,48 @@ var user1content = "";
 var user2content = "";
 var user1 = 0;
 var user2 = 0;
+var user1post = "";
+var user2post = "";
 
 /* Configure router to log */
 router.use(function(req, res, next){
-	console.log('/' + req.method);
+	console.log(req.method);
 	next();
 });
 
 router.get('/', function(req, res){
-		res.sendFile(__dirname + "/public/pad.html")
+	res.sendFile(__dirname + "/public/pad.html")
+});
+
+router.get('/users', function(req, res){
+	console.log(user1);
+	console.log(user2);
+});
+
+router.get('/strings', function(req, res){
+	console.log(user1content);
+	console.log(user2content);
+});
+
+router.post('/user', function(req, res){
+	if (user1 == 0){
+	 	user1 = req.body.id;
+		user1post = "/" + user1;
+		console.log(user1post);
+	} else if (user2 == 0){
+		user2 = req.body.id;
+	}
+	console.log(req.body);
 });
 
 router.post('/', function(req, res){
-	user1 = req.body.id;
-	console.log(req.body);
+	if (req.body.id == user1){
+		user1content = req.body.contents;
+	} else if (req.body.id == user2){
+		user2content = req.body.contents;
+	}
+	console.log(req.body.contents);
 });
-//
-// var getContents = function(){
-//
-// }
-// var getPads = function(user1, user2){
-//
-//
-// }
+
 
 module.exports = router;
